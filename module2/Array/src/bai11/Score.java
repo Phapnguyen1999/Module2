f772e047f0309c84e57eac4e1229f5fe1a857c87
Phapnguyen1999 <phapnguyen.2015@gmail.com>
2022-06-07 17:19:16 +0700

    add

---
+++ module2/Array/src/bai11/Score.java
@@ -0,0 +1,30 @@
+package bai11;
+
+import java.util.Scanner;
+
+public class Score {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size;
+        do {
+            System.out.print("Enter size: ");
+            size=scanner.nextInt();
+            if (size>30||size<1)
+                System.out.println("Enter size again!");
+        } while (size>30||size<1);
+        int array[]=new int[size];
+        for (int i=0;i<array.length;i++) {
+            System.out.print("Enter score for student " + (i + 1) + ": ");
+            array[i]= scanner.nextInt();
+        }
+        int count=0;
+        System.out.println("List score: ");
+        for (int i:array){
+            System.out.print(i+" ");
+            if (i >=5 && i<=10)
+                count++;
+        }
+        System.out.println();
+        System.out.print("The number of students pass the exam is: "+count);
+    }
+}
