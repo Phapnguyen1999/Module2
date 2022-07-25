94a0a5e87ca36df6d74da8da65e172d06c9e99f0
Phapnguyen1999 <phapnguyen.2015@gmail.com>
2022-07-25 09:38:36 +0700

    update

--- module2/Text_File/src/bai1/ReadFile.java
+++ module2/Text_File/src/bai1/ReadFile.java
@@ -1,2 +1,32 @@
-package bai1;public class ReadFile {
+package bai1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFile {
+    public void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader((file)));
+            String line ="";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tổng = " + sum);
+        } catch (Exception ex) {
+            System.out.println("Lỗi file");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ReadFile readFile = new ReadFile();
+        readFile.readFileText("src/bai1/text.txt");
+    }
 }
--- module2/Text_File/src/bai2/ReadAndWriteFile.java
+++ module2/Text_File/src/bai2/ReadAndWriteFile.java
@@ -1,2 +1,48 @@
-package bai2;public class ReadAndWriteFile {
+package bai2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath){
+        List<Integer> numbers= new ArrayList<>();
+        File file = new File(filePath);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line="";
+            while ((line=br.readLine())!=null){
+                numbers.add(Integer.parseInt(line));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Lo  i file");
+        }
+        return numbers;
+    }
+    public void writeFile(String filePath, int max){
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("src/bai2/numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("src/bai2/numbers.txt", maxValue);
+    }
 }
